cb84e2c3933e5b98a1f7e5d23ee9e7160e4f93a0
ksaid0203 <ksaid0203@gmail.com>
2019-05-06 21:36:26 +0900

    #BAK4-29

--- WebContent/solvedproblemGraph2.jsp
+++ WebContent/solvedproblemGraph2.jsp
@@ -6,16 +6,47 @@
 	Database db = new Database();
 	request.setCharacterEncoding("UTF-8");
 	String userid = request.getParameter("userId");
+	ArrayList<String[]> ans = db.readUserdata(userid, "solvedproblem");
 %>
+
 <!DOCTYPE html>
 <html>
 <head>
-<script>
-</script>
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+	    google.charts.load('current', {'packages':['line']});
+	    google.charts.setOnLoadCallback(drawChart);
+	
+		function drawChart() {
+	
+		    var data = new google.visualization.DataTable();
+		    data.addColumn('string', 'date');
+		    data.addColumn('number', '푼 문제 수');
+		    
+		    <%
+			    for(int i = 0 ; i < ans.size(); ++i) {
+			    	out.println("data.addRow([\'" +ans.get(i)[0] + "\', " + ans.get(i)[2] + "]);");
+			    }
+		    %>
+		    
+		    var options = {
+				chart: {
+		        	title: '<%=userid%>' + '님이 푼 문제 수 그래프',
+				},
+		      	width: 900,
+		      	height: 500
+		    };
+		
+		    var chart = new google.charts.Line(document.getElementById('linechart_material'));
+		
+		    chart.draw(data, google.charts.Line.convertOptions(options));
+	    }
+    </script>
 <meta charset="UTF-8">
 <title>데이터베이스 정보 읽어오기</title>
 </head>
 <body>
-	
+	<p id="data"> </p>
+	<div id = 'linechart_material'></div>
 </body>
 </html>
--- src/web/Database.java
+++ src/web/Database.java
@@ -1,7 +1,10 @@
 package web;
 
 import java.sql.*;
-
+import java.util.*;
+// jsp에서 쓸 때 먼저 db 객체를 만들어서 db 연결을 수립
+// db.selectUserInformations로 user의 원하는 정보 들고오고
+// db.getClosed()로 수립된 연결 해제
 public class Database {
 	private Connection con = null;
 	private final String DBNAME = "postgres";
@@ -19,11 +22,11 @@ public class Database {
 			con = DriverManager.getConnection("jdbc:postgresql://localhost:5434/postgres", DBNAME, PASSWORD);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			System.out.println("helloworld");
+			System.out.println("error occured");
 			e.printStackTrace();
 		}
 	}
-	
+	// db 연결 해제
 	public void getClosed() {
 		try {
 			con.close();
@@ -32,32 +35,26 @@ public class Database {
 			e.printStackTrace();
 		}
 	}
-	
-	public String selectUserInformations(final String userID, final String infoType) {
-		String ret = "";
+	// 정보 받아오기
+	public ArrayList<String[]> readUserdata(final String userID, final String infoType) {
+		ArrayList<String[]> ret = new ArrayList<String[]>();
 		try {
 			Statement stmt = con.createStatement();
-			String selectSQL = "SELECT * FROM Users WHERE userid=" + "\'" + userID + "\';";
+			String selectSQL = "SELECT date, list, numberofproblem\r\n" + 
+					"FROM " + infoType + "\r\n" + 
+					"WHERE userid = \'" + userID + "\'\r\n" + 
+					"ORDER BY date DESC\r\n" + 
+					"LIMIT 30;";
 			ResultSet resultSet = stmt.executeQuery(selectSQL);
 			while(resultSet.next()) {
-				ret = ret.concat(resultSet.getString(infoType));
+				ret.add(new String[] {resultSet.getString("date"), resultSet.getString("list"), resultSet.getString("numberofproblem")});
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			ret = userID + "님의 정보가 없습니다.";
+			ret.clear();
 			e.printStackTrace();
 		}
 		return ret;
 	}
-	// jsp에서 쓸 때 먼저 db 객체를 만들어서 db 연결을 수립
-	// db.selectUserInformations로 user의 원하는 정보 들고오고
-	// db.getClosed()로 수립된 연결 해제
-	public static void main(String[] args) {
-		Database db = new Database();
-		String ans1 = db.selectUserInformations("ksaid0203", "solvedproblem");
-		String ans2 = db.selectUserInformations("ksaid0203", "unsolvedproblem");
-		System.out.println(ans1);
-		System.out.println(ans2);
-		db.getClosed();
-	}
+
 }
